06c50bcb08fa938b621460784d0e1ab10ff629a7
GODOFTHEMETAL <uwontrememberthis@gmail.com>
2016-06-16 19:10:17 +0300

    cmt

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  entry: "./index",
+  output: {
+    path: __dirname + "/dist",
+    filename: "build.js"
+  },
+  watch: NODE_ENV === 'development',
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  devtool: NODE_ENV === 'development' ? "source-map" : null,
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query: {
+        presets: ["es2015", "stage-0"],
+        plugins: ["transform-runtime"]
+      }
+    }]
+  }
+}
