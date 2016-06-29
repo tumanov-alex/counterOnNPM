ae198e2db472835cc2d6f4a4ebd235e3b5914635
GODOFTHEMETAL <uwontrememberthis@gmail.com>
2016-06-29 19:27:20 +0300

    Z

--- index.js
+++ index.js
@@ -1,116 +1,127 @@
-import sha256 from 'sha256'
-import thunk from 'redux-thunk'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import sha256 from 'sha256';
+import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import createLogger from 'redux-logger';
 
-const logger = createLogger()
-const SET_SHAPES = 'SET_SHAPES'
-const BROKEN_LINK = 'BROKEN_LINK'
+const logger = createLogger();
+const SET_SHAPES = 'SET_SHAPES';
+const BROKEN_LINK = 'BROKEN_LINK';
 
 const rootReducer = (state = [], action) => {
   switch (action.type) {
     case SET_SHAPES:
       return [
-        ...action.payload
-      ]
+        ...action.payload,
+      ];
     case BROKEN_LINK:
-      console.error('The link is broken. Error: ' + action.err)
+      console.error(`The link is broken. Error: ${action.err}`);
       return [
-        action.err
-      ]
+        action.err,
+      ];
+    default:
+      return state;
   }
-}
+};
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk, logger)
-)
+);
 
-const getLink = () => {
-  return fetch('https://api.shamandev.com').then(
+const getLink = () => fetch('https://api.shamandev.com')
+  .then(
     data => data.text(),
     error => console.error(error)
-  )
-}
+  );
 
 const setShapes = (txt) => {
-  let msg = sha256(txt).replace(/\D+/g, '')
+  let msg = sha256(txt).replace(/\D+/g, '');
 
   for (let i = msg.length; i > 0; i--) {
     if (msg[i] > 4 && msg[i] < 100) {
-      msg = msg[i]
-      break
+      msg = msg[i];
+      break;
     }
   }
 
-  let arr = []
+  const arr = [];
   while (msg > 0) {
-    arr.push(Math.floor(Math.random() * 99) + 1)
-    msg--
+    arr.push(Math.floor(Math.random() * 99) + 1);
+    msg--;
   }
 
   return {
     type: SET_SHAPES,
-    payload: arr
-  }
-}
+    payload: arr,
+  };
+};
 
 const brokenLink = err => {
   return {
     type: BROKEN_LINK,
-    err: err
-  }
-}
+    err,
+  };
+};
 
-const sortShapes = function (){
+function sortShapes(...args) {
   return {
     type: SET_SHAPES,
-    payload: arguments[0].slice().sort((a, b) => {
-      return a - b
-    })
-  }
+    payload: args[0].slice().sort((a, b) => a - b),
+  };
 }
 
-const getShapes = () => {
-  return dispatch => {
-    return getLink().then(
-      link => dispatch(setShapes(link)),
-      err => dispatch(brokenLink(err))
-    )
-  }
-}
+const getShapes = () => dispatch => getLink()
+  .then(
+    link => dispatch(setShapes(link)),
+    err => dispatch(brokenLink(err))
+  );
 
 store.dispatch(
   getShapes()
-)
+);
 
 const App = (
   props
 ) => {
-  let result
-  let sum = -190
-  let viewBox = ''
-  let fontSize
-  let sortHandler
-  let resetHandler = store.dispatch.bind(null, getShapes())
+  let result;
+  let sum = -190;
+  let viewBox = '';
+  let fontSize;
+  let sortHandler;
+  let resetHandler = store.dispatch.bind(null, getShapes());
 
   if (props.shapes) {
-    result = props.shapes.map((radius, i)=> {
-      sum += radius + 200
-      fontSize = radius * 1.5
-      return <g key={i}>
-        <circle r={radius} cx={sum} cy="100" fill="#333" stroke="black" />
-        <text x={sum - fontSize/2} y={100 + fontSize/3} fill="white" style={{fontSize: fontSize}}>{radius}</text>
-      </g>
-    })
+    result = props.shapes.map((radius, i) => {
+      sum += radius + 200;
+      fontSize = radius * 1.5;
+      return (
+        <g key={i}>
+          <circle
+            r={radius}
+            cx={sum}
+            cy="100"
+            fill="#333"
+            stroke="black"
+          />
+          <text
+            x={sum - fontSize / 2}
+            y={100 + fontSize / 3}
+            fill="white"
+            style={{ fontSize }}
+          >
+            {radius}
+          </text>
+        </g>
+      );
+    });
 
-    sortHandler = store.dispatch.bind(null, sortShapes.bind(null, props.shapes)())
+    sortHandler = store.dispatch.bind(null, sortShapes.bind(null, props.shapes)());
   }
-  sum *= 1.1
-  sum = Math.abs(sum)
-  viewBox += '0 0 ' + sum.toString() + ' 200'
+  sum *= 1.1;
+  sum = Math.abs(sum);
+  viewBox += `0 0 ${sum.toString()} 200`;
 
   return (
     <div>
@@ -120,15 +131,15 @@ const App = (
       <button onClick={sortHandler}>SORT</button>
       <button onClick={resetHandler}>RESET</button>
     </div>
-  )
-}
+  );
+};
 
 const render = () => {
   ReactDOM.render(
     <App shapes={store.getState()} />,
     document.getElementById('root')
-  )
-}
+  );
+};
 
-store.subscribe(render)
-render()
+store.subscribe(render);
+render();
